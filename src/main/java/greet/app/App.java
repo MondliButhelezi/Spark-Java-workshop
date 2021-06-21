627ba09336fc2dddd350a949989eb44f2c01725e
Vuyo_M_ Buthelezi <vuyanimondlibuthelezi@gmail.com>
2021-06-21 18:34:42 +0200

    added handle bars

--- src/main/java/greet/app/App.java
+++ src/main/java/greet/app/App.java
@@ -6,16 +6,23 @@ public class App {
     public static void main(String[] args) {
 
         port(4040);
-        get("/greet", (request, response) -> "Dumela!");
+        get("/greet", (request, response) -> "Dumela! Welcome to my first Java web App ^_^ ");
 
         get("/greet/:username", (request, response) -> {
-            return "Dumela: " + request.params(":username");
+            return "Dumela, " + request.params(":username");
         });
 
 //        get("/greet/:username/language/:language", (request, response) -> {
 //            return "Dumela: " + request.params(":username");
 //        });
+        post("/greet", (request, response) -> {
+            if (request.queryParams("username").equals("username")) {
+                return request.queryParams("username");
+            } else {
+                return "Dumela!";
+            }
 
+        });
 
     }
 }
