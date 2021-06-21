251df2d056c93dd16c8802b01e35ab3efb443f36
Vuyo_M_ Buthelezi <vuyanimondlibuthelezi@gmail.com>
2021-06-21 18:31:38 +0200

    added index and port

--- src/main/java/greet/app/App.java
+++ src/main/java/greet/app/App.java
@@ -4,14 +4,18 @@ import static spark.Spark.*;
 public class App {
 
     public static void main(String[] args) {
+
+        port(4040);
         get("/greet", (request, response) -> "Dumela!");
 
         get("/greet/:username", (request, response) -> {
             return "Dumela: " + request.params(":username");
         });
 
-        get("/greet/:username/language/:language", (request, response) -> {
-            return "Dumela: " + request.params(":username");
-        });
+//        get("/greet/:username/language/:language", (request, response) -> {
+//            return "Dumela: " + request.params(":username");
+//        });
+
+
     }
 }
